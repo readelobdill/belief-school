8b6a08470aa04346182aa076977adacd9c17258f
Daniel Bonham <daniel@gladeye.co.nz>
2015-07-30 14:35:42 +1200

    fix array references in home module share

--- resources/views/app/share-modules/home.blade.php
+++ resources/views/app/share-modules/home.blade.php
@@ -2,23 +2,23 @@
 @section('content')
 <div class="content">
     <p>
-        I am <em>{{$home->pivot->data[0]->gender}}</em> and I am <em>{{$home->pivot->data[0]->age}} years old.</em>
+        I am <em>{{$module->pivot->data->{'1'}->gender}}</em> and I am <em>{{$module->pivot->data->{'1'}->age}} years old.</em>
     </p>
     <p>
-        I want <em>{{$home->pivot->data[0]->want}}</em>.
+        I want <em>{{$module->pivot->data->{'1'}->want}}</em>.
     </p>
     <p>
-        Because <em>{{$home->pivot->data[0]->why}}</em>.
+        Because <em>{{$module->pivot->data->{'1'}->why}}</em>.
     </p>
     <p>
-        When I have it I will feel <em>{{$home->pivot->data[0]->how}}</em>.
+        When I have it I will feel <em>{{$module->pivot->data->{'1'}->how}}</em>.
     </p>
     <p>
-        I don't have this because <em>{{$home->pivot->data[0]->why_not}}</em>.
+        I don't have this because <em>{{$module->pivot->data->{'1'}->why_not}}</em>.
     </p>
 
-    @if($welcome)
-        <p>Throughout my Belief School journey I chose to <em>{{$welcome->pivot->data[0]->challenge}}</em></p>
+    @if(isset($welcome))
+        <p>Throughout my Belief School journey I chose to <em>{{$welcome->pivot->data->{'1'}->challenge}}</em></p>
     @endif
 </div>
 @endsection
