02dd14bb16d6d672f2f06fa37eb334a567d64ef4
James Hopkins <james.hopkins@gladeye.co.nz>
2015-08-10 13:33:01 +1200

    dreamboard markup for mobile

--- resources/views/app/modules/visualise/dreamboard.blade.php
+++ resources/views/app/modules/visualise/dreamboard.blade.php
@@ -4,30 +4,38 @@
             Beautiful<br>
             you
         </h1>
-        
+
         <div class="board">
             <div class="row-4">
-                @include('app.modules.visualise.image', ['imageName' => 'image_1'])
-                @include('app.modules.visualise.image', ['imageName' => 'image_2'])
-                @include('app.modules.visualise.image', ['imageName' => 'image_3'])
-                @include('app.modules.visualise.image', ['imageName' => 'image_4'])
+                <div class="col">
+                    @include('app.modules.visualise.image', ['imageName' => 'image_1'])
+                    @include('app.modules.visualise.image', ['imageName' => 'image_2'])
+                </div>
+                <div class="col">
+                    @include('app.modules.visualise.image', ['imageName' => 'image_3'])
+                    @include('app.modules.visualise.image', ['imageName' => 'image_4'])
+                </div>
             </div>
             <div class="mid-row">
-                <div class="column">
+                <div class="col">
                     @include('app.modules.visualise.image', ['imageName' => 'image_5'])
                     @include('app.modules.visualise.image', ['imageName' => 'image_6'])
                 </div>
                 @include('app.modules.visualise.image', ['imageName' => 'image_main'])
-                <div class="column">
+                <div class="col">
                     @include('app.modules.visualise.image', ['imageName' => 'image_7'])
                     @include('app.modules.visualise.image', ['imageName' => 'image_8'])
                 </div>
             </div>
             <div class="row-4">
-                @include('app.modules.visualise.image', ['imageName' => 'image_9'])
-                @include('app.modules.visualise.image', ['imageName' => 'image_10'])
-                @include('app.modules.visualise.image', ['imageName' => 'image_11'])
-                @include('app.modules.visualise.image', ['imageName' => 'image_12'])
+                <div class="col">
+                    @include('app.modules.visualise.image', ['imageName' => 'image_9'])
+                    @include('app.modules.visualise.image', ['imageName' => 'image_10'])
+                </div>
+                <div class="col">
+                    @include('app.modules.visualise.image', ['imageName' => 'image_11'])
+                    @include('app.modules.visualise.image', ['imageName' => 'image_12'])
+                </div>
             </div>
             <div class="overlay">
 
