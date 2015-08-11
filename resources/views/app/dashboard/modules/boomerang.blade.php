bdd68a6cee9114143c6cc19c7177e1edf12498b9
James Hopkins <james.hopkins@gladeye.co.nz>
2015-08-12 08:49:41 +1200

    dashboard markup

--- resources/views/app/dashboard/modules/boomerang.blade.php
+++ resources/views/app/dashboard/modules/boomerang.blade.php
@@ -1,6 +1,13 @@
 <div class="content">
+    <p class="center">My trusted friends &amp; family think that I am...</p>
+    <div class="dots"></div>
+
     <script class="tagcloud-words" type="application/json">
         {!! json_encode($module->pivot->data) !!}
     </script>
     <div class="tagcloud"></div>
+
+     <p class="center">
+        <a class="button small-dashboard">Re-send email</a>
+    </p>
 </div>
--- resources/views/app/dashboard/modules/home.blade.php
+++ resources/views/app/dashboard/modules/home.blade.php
@@ -1,9 +1,17 @@
 <div class="header">
-    <h2>My Belief School Beginnings</h2>
-    <div class="options">
-
+    <div class="inner">
+        <h2>My Belief School Beginnings</h2>
+        <ul class="actions">
+            <li class="forum">
+                <a class="forum-icon" href="#">
+                    <span>Forum</span>
+                </a>
+            </li>
+            <li class="arrow"></li>
+        </ul>
     </div>
 </div>
+
 <div class="content">
     <p>
         I am <em>{{$home->pivot->data->{'1'}->gender}}</em> and I am <em>{{$home->pivot->data->{'1'}->age}} years old.</em>
