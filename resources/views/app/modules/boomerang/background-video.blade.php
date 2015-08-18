b4926b4d8bdfdb21360e876f6b81bb7ec5cdb564
James Hopkins <james.hopkins@gladeye.co.nz>
2015-08-18 20:33:26 +1200

    adding titles to intro pages for each module

--- resources/views/app/modules/boomerang/background-video.blade.php
+++ resources/views/app/modules/boomerang/background-video.blade.php
@@ -6,12 +6,13 @@
     </video>
 </div>
 <div class="inner">
-
     <div class="module-title">
         <h1 class="title">
             <span data-arc="90">&middot; Module {{$module->order - 1}} &middot;</span>
             {{$module->name}}
         </h1>
+
+        <div class="module-icon module-{{$module->slug}}"></div>
     </div>
 
     <div class="next-section" data-next-section>
--- resources/views/app/modules/you-to-you/background-video.blade.php
+++ resources/views/app/modules/you-to-you/background-video.blade.php
@@ -6,7 +6,15 @@
     </video>
 </div>
 <div class="inner">
-    <img src="{{asset('img/home-banner.png')}}" class="home-banner">
+    <div class="module-title">
+        <h1 class="title">
+            <span data-arc="90">&middot; Module {{$module->order - 1}} &middot;</span>
+            {{$module->name}}
+        </h1>
+
+        <div class="module-icon module-{{$module->slug}}"></div>
+    </div>
+
     <div class="next-section" data-next-section>
         @include('app/partials/icons/down-arrow')
     </div>
