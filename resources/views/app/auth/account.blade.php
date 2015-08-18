c3845c03cbd2519f32ec7f0da9d8a8d1bed9fc66
James Hopkins <james.hopkins@gladeye.co.nz>
2015-08-18 20:39:25 +1200

    account styles

--- resources/views/app/auth/account.blade.php
+++ resources/views/app/auth/account.blade.php
@@ -1,38 +1,46 @@
 @extends('app.layout')
 
 @section('content')
-    <div class="module">
-        <section class="account-section">
-            <div class="inner">
-                <div class="content">
-                    <form action="{{route('account.submit')}}" method="POST">
-                        <div class="form-row">
-                            <input type="text" name="first_name" required value="{{$user->first_name}}">
-                            <label>First Name</label>
-                        </div>
-                        <div class="form-row">
-                            <input type="text" name="last_name" required value="{{$user->last_name}}">
-                            <label>Last Name</label>
-                        </div>
-                        <div class="form-row">
-                            <input type="email" name="email" required value="{{$user->email}}" data-parsley-remote="{{route('account.check-email')}}" data-parlsey-remote-reverse="true">
-                            <label>Email Address</label>
-                        </div>
-                        <div class="form-row">
-                            <input type="text" name="username" required value="{{$user->username}}">
-                            <label>Username</label>
-                        </div>
-                        <div class="form-row">
-                            <input type="password" name="password" minlength="8">
-                            <label>Password</label>
-                        </div>
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                        <div class="actions">
-                            <button class="button">Save Details</button>
-                        </div>
-                    </form>
+    <div class="container account">
+        <div class="inner">
+                <header>
+                    <h1 class="title">
+                        Edit your account
+                    </h1>
+                </header>
+
+            <section class="account-section">
+                <div class="inner">
+                    <div class="content">
+                        <form action="{{route('account.submit')}}" method="POST">
+                            <div class="form-row">
+                                <input type="text" name="first_name" class="has-content" required value="{{$user->first_name}}">
+                                <label>First Name</label>
+                            </div>
+                            <div class="form-row">
+                                <input type="text" name="last_name" class="has-content" required value="{{$user->last_name}}">
+                                <label>Last Name</label>
+                            </div>
+                            <div class="form-row">
+                                <input type="email" name="email" class="has-content" required value="{{$user->email}}" data-parsley-remote="{{route('account.check-email')}}" data-parlsey-remote-reverse="true">
+                                <label>Email Address</label>
+                            </div>
+                            <div class="form-row">
+                                <input type="text" name="username" class="has-content" required value="{{$user->username}}">
+                                <label>Username</label>
+                            </div>
+                            <div class="form-row">
+                                <input type="password" name="password" minlength="8">
+                                <label>Password</label>
+                            </div>
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <div class="actions">
+                                <button class="button">Save Details</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </div>
 @endsection
