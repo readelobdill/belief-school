1c24535d73d44522be8f1ce9126ba4b1aad31faa
Daniel Bonham <daniel@gladeye.co.nz>
2015-08-18 14:48:02 +1200

    make forms js global (labels)

---
+++ resources/assets/js/ui/forms.js
@@ -0,0 +1,35 @@
+import $ from 'jquery';
+
+class Forms {
+    constructor() {
+        this.setupLabels();
+        console.log('setup');
+    }
+
+
+    setupLabels() {
+        $('input').each((e) => {
+            this.formOnFocus(e);
+        })
+        $('body').on('focus','input',this.formOnFocus.bind(this));
+        $('body').on('keyup','input',this.formOnFocus.bind(this));
+        $('body').on('change','input',this.formOnFocus.bind(this));
+    }
+
+    formOnFocus(e) {
+        let field = $(e.currentTarget);
+
+        if( field.val() ){
+            field.addClass('has-content');
+        } else {
+            field.removeClass('has-content');
+        }
+
+    }
+}
+
+export default {
+    init() {
+        const forms = new Forms();
+    }
+}
