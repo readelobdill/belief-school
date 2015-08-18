928a02f0e446548b924feaa5f6ae77965c30400c
James Hopkins <james.hopkins@gladeye.co.nz>
2015-08-18 14:14:44 +1200

    Congrats messages

--- resources/views/app/modules/gratitude/congrats.blade.php
+++ resources/views/app/modules/gratitude/congrats.blade.php
@@ -1,11 +1,8 @@
 <div class="inner">
     <div class="pre-complete congrats-container">
         <div class="inner">
-            <h1 class="plain">
-                Continue to record what <br />you are grateful for...
-            </h1>
 
-            <p>Keep your Gratitude Diary going - try to write down what you are grateful for at least once a day, even if you only write one thing. Come back to your Belief School Dashboard to see the original 10 things you are grateful for if you are struggling to find inspiration.</p>
+            “Give thanks for a little and you will find a lot” ~Hausa Proverb
 
             <div class="dots"></div>
             <div class="actions">
@@ -15,7 +12,10 @@
     </div>
     <div class="post-complete congrats-container">
         <div class="inner">
-            <h1 class="plain">Congratulations you are awesome!</h1>
+            <h1 class="plain"> Continue to record what <br />you are grateful for...</h1>
+
+            <p>Keep your Gratitude Diary going - try to write down what you are grateful for at least once a day, even if you only write one thing. Come back to your Belief School Dashboard to see the original 10 things you are grateful for if you are struggling to find inspiration</p>
+
             <p>Your next module will unlock in 48 Hours.</p>
             <p>
                 In the meantime <a href="{{route('dashboard')}}">keep an eye on your dashboard</a> for the qualities submitted by your friends and <a href="{{route('modules.forum',[$module->slug])}}">check in the forum to find support and chat to others about getting to know your amazing self.</a>
--- resources/views/app/modules/un-stuck/congrats.blade.php
+++ resources/views/app/modules/un-stuck/congrats.blade.php
@@ -1,19 +1,13 @@
 <div class="inner">
     <div class="pre-complete congrats-container">
-        [[NOTE: There is no copy for this]]
-        {{-- <div class="inner">
-            <h1 class="plain">
-                Great job!<br>
-                Asking for feedback takes courage; you are
-                going to learn a lot about courage over
-                the course of your Belief School journey,
-            </h1>
-            <p>All of the answers will be collated and appear in your Dashboard once your recipients begin to send their answers, to you so you can start to build a picture of the awesomness you bring to the world.</p>
-            <div class="dots"></div>
+
+        <div class="inner">
+            [[NOTE: There is no copy for this]]
+
             <div class="actions">
                 <a href="#" class="button" data-complete-module>What's next?</a>
             </div>
-        </div> --}}
+        </div>
     </div>
     <div class="post-complete congrats-container">
         <div class="inner">
