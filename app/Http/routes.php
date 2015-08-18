4e98cf328fd27eb545496d138122667ff124c3d7
Daniel Bonham <daniel@gladeye.co.nz>
2015-08-19 11:07:51 +1200

    moved routes requiring auth into a group

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,24 +13,44 @@
 
 Route::get('/', ['as' => 'home', 'uses' => 'ModuleController@viewModule']);
 
-/*Modules*/
 
-
-
-Route::get('modules/visualise/dreamboard', ['as' => 'modules.visualise.dreamboard','uses' => 'ModuleController@showDreamboardImage']);
-
-Route::post('modules/{module}/update', ['as' => 'modules.update', 'uses' => 'ModuleController@updateModule']);
-Route::post('modules/{module}/complete', ['as' => 'modules.complete', 'uses' => 'ModuleController@completeModule']);
 Route::get('modules/{module}', ['as' => 'modules.view', 'uses' => 'ModuleController@viewModule']);
-Route::get('modules/{module}/forum', ['as' => 'modules.forum', 'uses' => 'ModuleController@viewForum']);
 
 
-Route::post('comments/{comment}/reply', ['as' => 'comments.reply', 'uses' => 'CommentController@reply']);
-Route::post('modules/{module}/comment', ['as' => 'modules.comment', 'uses' => 'CommentController@create']);
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('modules/{module}/forum', ['as' => 'modules.forum', 'uses' => 'ModuleController@viewForum']);
 
 
-Route::get('dashboard', ['as' => 'dashboard', 'uses' => 'DashboardController@index']);
+    Route::post('comments/{comment}/reply', ['as' => 'comments.reply', 'uses' => 'CommentController@reply']);
+    Route::post('modules/{module}/comment', ['as' => 'modules.comment', 'uses' => 'CommentController@create']);
 
+
+    Route::get('dashboard', ['as' => 'dashboard', 'uses' => 'DashboardController@index']);
+
+    Route::get('modules/visualise/dreamboard', ['as' => 'modules.visualise.dreamboard','uses' => 'ModuleController@showDreamboardImage']);
+
+    Route::post('modules/{module}/update', ['as' => 'modules.update', 'uses' => 'ModuleController@updateModule']);
+    Route::post('modules/{module}/complete', ['as' => 'modules.complete', 'uses' => 'ModuleController@completeModule']);
+
+
+
+    Route::get('account', ['as' => 'account', 'middleware' => 'auth', 'uses' => 'UserController@account']);
+    Route::post('account', ['as' => 'account.submit', 'middleware' => 'auth', 'uses' => 'UserController@submitAccount']);
+
+    Route::get('fake-pay', ['as' => 'payment', 'middleware' => 'auth',function() {
+        $user = Auth::user();
+        $user->paid = true;
+        $user->save();
+        $module = $user->modules()->where('slug', 'home')->first();
+        $module->pivot->step++;
+        $module->pivot->complete = 1;
+        $module->pivot->completed_at = new \Carbon\Carbon();
+        $module->pivot->save();
+
+        return redirect(route('modules.view', ['home']));
+    }]);
+});
 
 Route::get('tagcloud/{secret}', ['as' => 'tagcloud', 'uses' => 'ModuleController@tagCloud']);
 Route::post('tagcloud/{secret}', ['as' => 'tags.submit', 'uses' => 'ModuleController@tagCloudSubmit']);
@@ -54,18 +74,7 @@ Route::bind('comment', function($value) {
     return \App\Models\Comment::with(['user'])->where('id', $value)->first();
 });
 
-Route::get('fake-pay', ['as' => 'payment',function() {
-    $user = Auth::user();
-    $user->paid = true;
-    $user->save();
-    $module = $user->modules()->where('slug', 'home')->first();
-    $module->pivot->step++;
-    $module->pivot->complete = 1;
-    $module->pivot->completed_at = new \Carbon\Carbon();
-    $module->pivot->save();
 
-    return redirect(route('modules.view', ['home']));
-}]);
 
 Route::post('users', ['as' => 'users.create', 'uses' => 'UserController@createUser']);
 
@@ -81,8 +90,7 @@ Route::get('logout', ['as' => 'auth.logout','uses' => 'Auth\AuthController@getLo
 //});
 
 
-Route::get('account', ['as' => 'account', 'middleware' => 'auth', 'uses' => 'UserController@account']);
-Route::post('account', ['as' => 'account.submit', 'middleware' => 'auth', 'uses' => 'UserController@submitAccount']);
+
 Route::any('email-exists', ['as' => 'account.check-email', 'uses' => 'UserController@checkEmail']);
 
 Route::get('about', ['as' => 'about', function() {
