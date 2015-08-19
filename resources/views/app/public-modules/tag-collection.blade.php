0b8c4d1a83c449220d7bfe3019c2c6b8341b08c8
James Hopkins <james.hopkins@gladeye.co.nz>
2015-08-19 19:15:39 +1200

    login & account styles

--- resources/views/app/public-modules/tag-collection.blade.php
+++ resources/views/app/public-modules/tag-collection.blade.php
@@ -1,21 +1,35 @@
 @extends('app.public-layout')
 
 @section('content')
-    <form action="{{route('tags.submit', [$moduleUser->secret])}}" method="POST" class="tag-cloud-form">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="form-row">
-            <input type="text" name="tags[]">
-        </div>
-        <div class="form-row">
-            <input type="text" name="tags[]">
-        </div>
-        <div class="form-row">
-            <input type="text" name="tags[]">
-        </div>
-        <div class="actions">
-            <button class="button">Submit</button>
-        </div>
-    </form>
+    <div class="container non-mod-page">
+        <div class="inner">
+            <header>
+                <h1 class="title">
+                    Write a few words about {{$moduleUser->user->first_name}}
+                </h1>
+            </header>
 
-
+            <section class="tag-cloud">
+                   <div class="inner">
+                    <div class="content">
+                        <form action="{{route('tags.submit', [$moduleUser->secret])}}" method="POST" class="tag-cloud-form">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <div class="form-row">
+                                <input type="text" name="tags[]">
+                            </div>
+                            <div class="form-row">
+                                <input type="text" name="tags[]">
+                            </div>
+                            <div class="form-row">
+                                <input type="text" name="tags[]">
+                            </div>
+                            <div class="actions">
+                                <button class="button">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
 @endsection
