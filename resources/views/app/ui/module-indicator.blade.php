b5376835b58daf2144643ec95698d34f18ed7fbc
Daniel Bonham <daniel@gladeye.co.nz>
2015-08-20 11:45:52 +1200

    added module indicator link to forums

--- resources/views/app/ui/module-indicator.blade.php
+++ resources/views/app/ui/module-indicator.blade.php
@@ -1,9 +1,16 @@
 @if(!empty($module))
     <div class="indicator">
-        <a class="forum-icon" href="{{ route('modules.forum', [$module->slug]) }}">
-            @include('app/partials/icons/forum')
-            <span>Forums</span>
-        </a>
+        @if($page === 'forum')
+            <a class="forum-icon" href="{{ route('modules.view', [$module->slug]) }}">
+                <span>Module</span>
+            </a>
+        @else
+            <a class="forum-icon" href="{{ route('modules.forum', [$module->slug]) }}">
+                @include('app/partials/icons/forum')
+                <span>Forums</span>
+            </a>
+        @endif
+
         <div class="module-icon page-icon module-{{$module->slug}}">
 
         </div>
