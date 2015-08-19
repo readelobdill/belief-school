5ddd9fb75ccf67f41e75f4c08459623e4c9aa2fa
Daniel Bonham <daniel@gladeye.co.nz>
2015-08-20 09:53:22 +1200

    Added name to boomerang email

---
+++ resources/views/emails/boomerang.blade.php
@@ -0,0 +1,11 @@
+Dear Trusted Friend,
+
+{{$user->first_name}} is completing an online program called Belief School with the intention of developing their personal belief so they can create the life that they want. Jane has stepped out of her comfort zone and sent you this email because she values your opinion and trusts that you will answer the simple questions honestly and with her best interests at heart.
+
+Clicking on the link will take you to a page on our Belief School website where you can input the three amazing qualities you believe Janes has. The answers will be delivered to Jane anonymously mixed up with responses from up to 25 other friends, family and colleagues.
+
+Thanks for taking the time to answer these questions, it really does make a difference.
+
+Best regards
+
+Belief School
