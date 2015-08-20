7245eac619c3d89e06f8f7d2c2bf8efa6c75a16f
Daniel Bonham <daniel@gladeye.co.nz>
2015-08-21 09:32:13 +1200

    thank you page for tag collection styling

--- resources/views/app/public-modules/tag-collection-success.blade.php
+++ resources/views/app/public-modules/tag-collection-success.blade.php
@@ -1,9 +1,21 @@
 @extends('app.public-layout')
 
 @section('content')
-    <div class="content">
-        Thank you.
+    <div class="container non-mod-page">
+        <div class="inner">
+            <header>
+                <h1 class="title">
+                    Write a few words about {{$moduleUser->user->first_name}}
+                </h1>
+            </header>
+
+            <section class="tag-cloud">
+                <div class="inner">
+                    <div class="content">
+                        Thank you
+                    </div>
+                </div>
+            </section>
+        </div>
     </div>
-
-
 @endsection
