f6c8e5762c49683a0a5293399d456c63e5720ead
James Hopkins <james.hopkins@gladeye.co.nz>
2015-09-22 14:27:04 +1200

    Fixed down arrow, signup label color and added terms

--- resources/views/app/misc/privacy-terms.blade.php
+++ resources/views/app/misc/privacy-terms.blade.php
@@ -6,36 +6,123 @@
         <div class="inner">
 
             <div class="content">
-                <h1 class="plain">Terms of use</h1>
+                <h1 class="plain">Disclaimer</h1>
 
-                <div class="dots"></div>
+                <p>You expressly agree that your use or inability to use our Belief School product is at your own risk.</p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p>By enrolling in Belief School you agree and understand that you are fully responsible for your progress and the results you get. We offer no guarantees verbally or in writing with regards to your ability to create change in your life.</p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                <p>You also understand that any testimonials or endorsements by our customers or audience represented on our programs, websites, content, sales pages or social media channels have not been evaluated by us. The results experienced by any person participating in the Belief School Program may vary. </p>
 
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                <p>Any statements from any of the above platforms, our content and our coaching are simply our opinions and not a guarantee or promise of actual performance. We offer no psychological or counselling advice.</p>
             </div>
 
             <div class="content">
+                <h1 class="plain">Terms of Use for Belief School</h1>
 
-                <h1 class="plain">Privacy Policy</h1>
+                <p>We provide Visitors and Registered Users with access to the Services as described below.</p>
 
-                <div class="dots"></div>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-
-                <p><a href="#">link</a></p>
-
+                <p>Belief School is under no obligation to accept any individual as a Registered User and may accept or reject any registration in its sole and complete discretion. In addition, Belief School may deactivate any account at any time.</p>
             </div>
+
+            <div class="content">
+                <h1 class="plain">School/Community Guidelines</h1>
+
+                <p>The Belief School community functions best when its users follow a few simple rules. By using the Belief School program, you agree to comply with our community guidelines.</p>
+
+                <p>You will comply with all applicable laws in your use of the Websites and will not use the Websites for any unlawful purpose;</p>
+
+                <ol>
+                    <li>You will not harass or threaten, or another person.</li>
+                    <li>You will not use any of the content of Belief School to create a competing product or service.</li>
+                    <li>You will not impose, at our discretion an excessive large load on our technical infrastructure.</li>
+                    <li>You will not post, upload or email any content that is:
+                        <ul>
+                            <li>Indecent, pornographic or sexual in anyway, libellous, defamatory, violent or hateful in any way to any group based on their age, sexual orientation, religion or gender</li>
+                            <li>Publish any personal information from anyone else such as phone numbers, emails, or payment information.</li>
+                            <li>Infringe on anyone else’s copyright or trademarks</li>
+                        </ul>
+                    </li>
+                    <li>We reserve the right, in our sole and absolute discretion, to deny you access to Belief School, without any notice.</li>
+                    <li>We reserve the right to remove any of your content that does not adhere to our Community Guidelines.</li>
+                </ol>
+            </div>
+
+            <div class="content">
+                <h1 class="plain">Sign in and password</h1>
+
+                <p>At registration you will asked to create an account with a sign in name and password. This information must be truthful. This sign in name can be used by only one user. </p>
+            </div>
+
+            <div class="content">
+                <h1 class="plain">Restrictions</h1>
+
+                <p>Belief School is available to those 18 years and older. However someone between the age of 13 -16 can complete the school with guardian approval.</p>
+            </div>
+
+            <div class="content">
+                <h1 class="plain">Fees &amp; Payment</h1>
+
+                <p>As consideration for any purchase you make on the Websites, you shall pay Belief School Limited all applicable fees. We (or our third-party payment processor) shall authorize your credit card, bank account, or other approved payment facility you provided during the registration process for the full payment of the fees and any applicable taxes, and you hereby consent to the same. </p>
+
+                <p>If the mailing address you provided us is in the New Zealand Dollars, all payments will be charged and made in New Zealand dollars. If the mailing address you provided us is outside New Zealand, all payments will be charged and made in your local currency. (need this confirmed?)</p>
+            </div>
+
+            <div class="content">
+                <h1 class="plain">Intellectual Property</h1>
+
+                <p>The Websites contain material, such as videos, coursework, lesson plans, training modules, photographs, software, text, graphics, images, sound recordings, and other material provided by or on behalf Paula Gosney and Belief School. The Content may be owned by us or third parties. The Content is protected under New Zealand and International Law. Unauthorized use of the Content may violate copyright, trademark, and other laws.</p>
+
+                <p>Registered users may view all content for their own personal, non-commercial use. No other use is permitted without the prior written consent of Belief School Limited. Belief School retains all right, title, and interest, including all intellectual property rights, in and to the Content. You may not sell, transfer, assign, license, sublicense, or modify the Content or reproduce, display, publicly perform, make a derivative version of, distribute, or otherwise use the Content in any way for any public or commercial purpose. The use or posting of the Content on any other website, social media page, or in a networked computer environment for any purpose is expressly prohibited.</p>
+
+                <p>If you violate any part of this Agreement, your permission to access and/or use the Content and the Websites automatically terminates and you must immediately destroy any copies you have made of the Content.</p>
+
+                <p>The trademarks, service marks, and logos of Belief School used and displayed on the Websites are registered and unregistered trademarks or service marks Belief School. </p>
+
+                <p>Nothing on the Websites should be construed as granting, by implication, estoppel, or otherwise, any license or right to use the Trademarks, without our prior written permission specific for each such use. Use of the Trademarks as part of a link to or from any site is prohibited unless establishment of such a link is approved in advance by us in writing. All goodwill generated from the use of Belief School Limited inures to our benefit.</p>
+            </div>
+
+            <div class="content">
+                <h1 class="plain">Registered User Content; licenses</h1>
+
+                <p>As noted above, the Websites provides Registered Users the ability to post and upload Registered User Content. You expressly acknowledge and agree that once you submit your Registered User Content for inclusion into the Websites, it will be accessible by others, and that there is no confidentiality or privacy with respect to such Registered User Content, including, without limitation, any personally identifying information that you may make available. YOU, AND NOT BELIEF SCHOOL LIMITED, ARE ENTIRELY RESPONSIBLE FOR ALL YOUR REGISTERED USER CONTENT THAT YOU UPLOAD, POST, EMAIL, OR OTHERWISE TRANSMIT VIA THE WEBSITES, AND YOU WILL INDEMNIFY US IN RESPECT OF ANY LOSS, LIABILITY, CLAIM, OR DAMAGE SUFFERED OR INCURRED BY US IN CONNECTION WITH YOUR REGISTERED USER CONTENT.</p>
+
+                <p>You retain all copyrights and other intellectual property rights in and to your own Registered User Content. You do, however, hereby grant us and our sublicensees a non-exclusive, royalty-free, freely sublicensable, perpetual license to modify, compile, combine with other content, copy, record, synchronize, transmit, translate, format, distribute, publicly display, publicly perform, and otherwise use or exploit (including for profit) your Registered User Content and all intellectual property and moral rights therein throughout the universe, in each case, by or in any means, methods, media, or technology now known or hereafter devised. You also grant us and our sublicensees the right, but not the obligation, to use your Registered User Content, your Sign-In Name, name, likeness, and photograph in connection with any use of the related Registered User Content permitted by the previous sentence and/or to advertise and promote the Websites, Belief School, and our products and services. Without limiting the foregoing, you acknowledge and agree that uses of your Registered User Content, name, likeness, and photograph permitted by the foregoing rights and licenses may include the display of such Registered User Content, name, likeness, and photograph adjacent to advertising and other material or content, including for profit.</p>
+
+                <p>If you submit Registered User Content to us, each such submission constitutes a representation and warranty to Belief School that such Registered User Content is your original creation (or that you otherwise have the right to provide the Registered User Content), that you have the rights necessary to grant the license to the Registered User Content under the prior paragraph, and that it and its use by Belief School Limited and its content partners as permitted by this Agreement does not and will not infringe or misappropriate the intellectual property or moral rights of any person or contain any libelous, defamatory, or obscene material or content that violates our Community Guidelines.</p>
+            </div>
+
+            <div class="content">
+                <h1 class="plain">No Warranties; Limitation Of Liability</h1>
+
+                <p>The websites and the content are provided on an “as is” and “as available” basis without any warranties of any kind, including that the websites will operate error-free or that the websites, their servers, or the content are free of computer viruses or similar contamination or destructive features.</p>
+
+                <p>To the maximum extent permitted by applicable laws: (i) we shall not be subject to liability for any delays or interruptions of the website from whatever cause, and you agree that you use the content and the websites at your own risk; (ii) we disclaim all warranties, including, but not limited to, warranties of title, merchantability, non-infringement of third parties’ rights, and fitness for particular purpose and any warranties arising from a course of dealing, course of performance, or usage of trade; (iii) we shall not be liable for any damages whatsoever (including, without limitation, direct, incidental and consequential damages, lost profits, or damages resulting from lost data or business interruption) resulting from the use or inability to access and use the websites or the content, whether based on warranty, contract, tort (including negligence), or any other legal theory, even if we have been advised of the possibility of such damages.</p>
+
+                <p>If your use of the websites or the content results in the need for servicing or replacing equipment or data, we shall not be responsible for those costs unless required by applicable laws.</p>
+
+                <p>The websites may contain technical inaccuracies or typographical errors or omissions. Unless required by applicable laws, we are not responsible for any such typographical, technical, or pricing errors listed on the websites. We reserve the right to make changes, corrections, and/or improvements to the websites at any time without notice.</p>
+            </div>
+
+            <div class="content">
+                <h1 class="plain">Representations; Warranties; and Indemnification</h1>
+
+                <p>(a) If you are a Registered User, you hereby represent, warrant, and covenant that:</p>
+
+                <p>You own or have the necessary licenses, rights, consents, and permissions to all trademark, trade secret, copyright, or other proprietary, privacy, and publicity rights in and to your Registered User Content and any other works that you incorporate into your Registered User Content, and all the rights necessary to grant the licenses and permissions you grant hereunder;</p>
+
+                <p>Use of your Registered User in the manners contemplated in this Agreement shall not violate or misappropriate the intellectual property, privacy, publicity, contractual, or other rights of any third party; and</p>
+
+                <p>You shall not submit to the Website any Registered User Content that violates our Community Guidelines set forth above or any other term of this Agreement.</p>
+
+                <p>(b) You agree to defend, indemnify, and hold us and our officers, directors, employees, agents, successors, licensees, and assigns harmless from and against any damages, liabilities, losses, expenses, claims, actions, and/or demands, including, without limitation, reasonable legal and accounting fees, arising or resulting from: (i) your breach of this Agreement; (ii) your access to, use or misuse of the Content or the Websites; and/or (iii) your violation of any third-party rights, including without limitation any copyright, trademark, property, publicity, or privacy right. We shall provide notice to you of any such claim, suit, or proceeding and shall assist you, at your expense, in defending any such claim, suit, or proceeding. We reserve the right to assume the exclusive defense and control (at your expense) of any matter that is subject to indemnification under this section. In such case, you agree to cooperate with any reasonable requests assisting our defense of such matter.</p>
+            </div>
+
+            <div class="content">
+                <h1 class="plain">Termination of The Agreement</h1>
+                <p>We reserve the right, in our sole discretion, to restrict, suspend, or terminate this Agreement and your access to all or any part of the Websites, at any time and for any reason without prior notice or liability. We reserve the right to change, suspend, or discontinue all or any part of the Websites at any time without prior notice or liability.</p>
+            </div>
+
         </div>
     </div>
 
