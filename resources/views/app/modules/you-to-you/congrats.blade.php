149b52f21dbfb2d09a2109b3b211f6fd50dcc64e
Daniel Bonham <daniel@gladeye.co.nz>
2015-09-25 15:21:09 +1200

    add content wrapper to module 8 congrats page

--- resources/views/app/modules/you-to-you/congrats.blade.php
+++ resources/views/app/modules/you-to-you/congrats.blade.php
@@ -1,19 +1,24 @@
 <div class="inner">
     <div class="pre-complete congrats-container">
         <div class="inner">
-            <h1 class="plain">
-                Congratulations, you've made it through!
-            </h1>
-            <p>You've now completed your journey through Belief School. Come back from time to time to get inspiration from your dashboard and encouragement from our forums. This is your supportive community,</p>
-            <div class="actions">
-                <a href="#" class="button" data-complete-module>Save to dashboard!</a>
+            <div class="content">
+                <h1 class="plain">
+                    Congratulations, you've made it through!
+                </h1>
+                <p>You've now completed your journey through Belief School. Come back from time to time to get inspiration from your dashboard and encouragement from our forums. This is your supportive community,</p>
+                <div class="actions">
+                    <a href="#" class="button" data-complete-module>Save to dashboard!</a>
+                </div>
             </div>
         </div>
     </div>
     <div class="post-complete congrats-container">
+
         <div class="inner">
-            <p>When heading over to your <strong>dashboard</strong>, take a moment to save it out - send it to your trusted friends and family and share your journey.</p>
-            <p class="center"><a href="{{route('dashboard')}}#module-{{$module->slug}}" class="button">Check it out now!</a></p>
-        </div>
+            <div class="content">
+                <p>When heading over to your <strong>dashboard</strong>, take a moment to save it out - send it to your trusted friends and family and share your journey.</p>
+                <p class="center"><a href="{{route('dashboard')}}#module-{{$module->slug}}" class="button">Check it out now!</a></p>
+            </div>
+        </div>gulp
     </div>
 </div>
