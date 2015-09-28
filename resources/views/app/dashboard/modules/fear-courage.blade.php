7e56ee41f9a3666095148c659ed76680172ba6e5
James Hopkins <james.hopkins@gladeye.co.nz>
2015-09-28 14:00:48 +1300

    styled dashboard content for consistencey

--- resources/views/app/dashboard/modules/fear-courage.blade.php
+++ resources/views/app/dashboard/modules/fear-courage.blade.php
@@ -4,21 +4,21 @@
             <li>
                 <span>My fear is:</span>
                 {{ $module->pivot->data[0]->{'challenge-1'} }}
-                <span>confronting it made me feel</span>
+                <span>confronting it made me feel:</span>
                 {{ $module->pivot->data[1]->{'response-1'} }}
             </li>
 
             <li>
                 <span>My fear is:</span>
                 {{ $module->pivot->data[0]->{'challenge-2'} }}
-                <span>confronting it made me feel</span>
+                <span>confronting it made me feel:</span>
                 {{ $module->pivot->data[1]->{'response-2'} }}
             </li>
 
             <li>
                 <span>My fear is:</span>
                 {{ $module->pivot->data[0]->{'challenge-3'} }}
-                <span>confronting it made me feel</span>
+                <span>confronting it made me feel:</span>
                 {{ $module->pivot->data[1]->{'response-3'} }}
             </li>
 
