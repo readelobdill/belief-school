1c3bb68a7d6a0b53a11482fc8bd9275008ca79f0
Daniel Bonham <daniel@gladeye.co.nz>
2015-10-05 13:31:49 +1300

    refactors error functions to allow non parsley error display

--- resources/assets/js/util/errors.js
+++ resources/assets/js/util/errors.js
@@ -3,13 +3,21 @@ import 'util/parsley';
 import TweenMax from "gsap/src/uncompressed/TweenMax";
 
 export function showError(field, container, direction) {
-    direction = direction || 'left';
     let errors = ParsleyUI.getErrorsMessages(field);
-    let $controls = field.$element.closest(container);
-    console.log($controls);
+    showRawError(field.$element, errors[0], container, direction);
+}
+
+export function hideError(field, container) {
+    hideError(field.$element, container);
+}
+
+
+export function showRawError(field, error, container, direction) {
+    direction = direction || 'left';
+    let $controls = $(field).closest(container);
     let errorContainer = $controls.find('.error-container');
     if(!errorContainer.length) {
-        errorContainer = $('<div></div>').addClass('error-container').html(errors[0]);
+        errorContainer = $('<div></div>').addClass('error-container').html(error);
         $controls.append(errorContainer);
         switch(direction) {
             case 'left':
@@ -27,12 +35,12 @@ export function showError(field, container, direction) {
         }
 
     } else {
-        errorContainer.html(errors[0]);
+        errorContainer.html(error);
     }
 }
 
-export function hideError(field, container) {
-    let errorContainer = field.$element.closest(container).find('.error-container');
+export function hideRawError(field,  container) {
+    let errorContainer = field.closest(container).find('.error-container');
     TweenMax.to(errorContainer, 0.3, {opacity: 0, onComplete: () => {
         errorContainer.remove();
     }});
