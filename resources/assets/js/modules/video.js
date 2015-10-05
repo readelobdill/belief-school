f4b34dda7075b7408cafad8c5999d14ac6a1867b
Daniel Bonham <daniel@gladeye.co.nz>
2015-10-06 10:50:28 +1300

    fixed jump

--- resources/assets/js/modules/video.js
+++ resources/assets/js/modules/video.js
@@ -56,21 +56,6 @@ function Video(video) {
 
 }
 
-Video.prototype.getPercentLoaded = function() {
-    let percent = null;
-    if (this.video && this.video.buffered && this.video.buffered.length > 0 && this.video.buffered.end && this.video.duration) {
-        percent = this.video.buffered.end(0) / this.video.duration;
-    }
-    // Some browsers (e.g., FF3.6 and Safari 5) cannot calculate target.bufferered.end()
-    // to be anything other than 0. If the byte count is available we use this instead.
-    // Browsers that support the else if do not seem to have the bufferedBytes value and
-    // should skip to there. Tested in Safari 5, Webkit head, FF3.6, Chrome 6, IE 7/8.
-    else if (this.video && this.video.bytesTotal != undefined && this.video.bytesTotal > 0 && this.video.bufferedBytes != undefined) {
-        percent = this.video.bufferedBytes / this.video.bytesTotal;
-    }
-
-    return percent;
-}
 
 Video.prototype.videoHasLoaded = function() {
     $('body').removeClass('is-loading-video');
@@ -102,8 +87,9 @@ Video.prototype.seek = function(per) {
     if(!this.isVideo()) {
         return Q.resolve();
     }
+
     return this.videoReady.then(() => {
-        this.video.currentTime = this.video.duration * per;
+        this.video.currentTime = this.duration * per;
     })
 
 }
