066bac71d95797e67e31c92c424d5c1d94deb6b5
James Hopkins <james.hopkins@gladeye.co.nz>
2015-10-05 13:57:47 +1300

    re-do of questions markup

--- resources/views/app/modules/home/questions/4.blade.php
+++ resources/views/app/modules/home/questions/4.blade.php
@@ -1,39 +1,39 @@
-<div class="content">
-    <h1 class="title">
-        <span data-arc="150"><span class="mid-dot">&middot;</span> What do you want? <span class="mid-dot">&middot;</span></span>
-        What do<br>
-        you really want?
-    </h1>
-    <p>
-        Let's put it another way, what do you want if you believed
-        you could be, do or have anything?
-    </p>
-    <p>
-        Take a few minutes, what is that thought, that dream, that wish - yes
-        the one you keep dismissing because you think it is not possible for you.
-    </p>
-    <p>
-        What is the one thing you would love if there were no limitations,
-        no obstacles - no fear?
-    </p>
-    <p>
-        <strong>
-        Write a concise answer here as this information will be  included in
-        your beautiful Belief School Manifesto.
-        </strong>
-    </p>
-    <p>
-        <strong>
-        Top Tip: It's a great idea to start a Belief School Journal and
-        explore these questions in more depth.
-        </strong>
-    </p>
-    <div class="controls">
-        <form action="" data-validate>
-            <div class="form-row">
-                <textarea name="want" id="want" cols="30" rows="10" required maxlength="140">
-                </textarea>
-            </div>
-        </form>
+<div class="inner">
+    <div class="content">
+        <h1 class="title">
+            <span data-arc="150"><span class="mid-dot">&middot;</span> What do you want? <span class="mid-dot">&middot;</span></span>
+            What do<br>
+            you really want?
+        </h1>
+        <p>
+            Let's put it another way, what do you want if you believed
+            you could be, do or have anything?
+        </p>
+        <p>
+            Take a few minutes, what is that thought, that dream, that wish - yes
+            the one you keep dismissing because you think it is not possible for you.
+        </p>
+        <p>
+            What is the one thing you would love if there were no limitations,
+            no obstacles - no fear?
+        </p>
+        <p><strong>
+            Write a concise answer here as this information will be  included in
+            your beautiful Belief School Manifesto.</strong>
+        </p>
+        <p><strong>
+            Top Tip: It's a great idea to start a Belief School Journal and
+            explore these questions in more depth.</strong>
+        </p>
+        <div class="controls">
+            <form action="" data-validate>
+                <div class="form-row">
+                    <textarea name="want" id="want" cols="30" rows="10" required maxlength="140"></textarea>
+                </div>
+            </form>
+        </div>
+        <div class="next-question absol down-arrow">
+            @include('app/partials/icons/down-arrow')
+        </div>
     </div>
 </div>
--- resources/views/app/modules/home/questions/5.blade.php
+++ resources/views/app/modules/home/questions/5.blade.php
@@ -1,17 +1,21 @@
-<div class="content">
-    <h1 class="title">
-        Why do you want this?
-    </h1>
-    <p>This is important, understanding why you want the <br> 
-        things you do, will play a big part in achieving them.</p>
-    <p>Dig a little deeper with this, once you have your<br>
-        first answer, then ask yourself Why, again.</p>
-    <div class="controls">
-        <form action="" data-validate>
-            <div class="form-row">
-                <textarea name="why" id="why" cols="30" rows="10" required data-parsley-maxlength="140" data-parsley-minlength="1"></textarea>
-            </div>
-        </form>
-    </div>
+<div class="inner">
+    <div class="content">
+        <h1 class="title">Why do you want this?</h1>
 
+        <p>This is important, understanding why you want the <br>
+            things you do, will play a big part in achieving them.</p>
+        <p>Dig a little deeper with this, once you have your<br>
+            first answer, then ask yourself Why, again.</p>
+
+        <div class="controls">
+            <form action="" data-validate>
+                <div class="form-row">
+                    <textarea name="why" id="why" cols="30" rows="10" required data-parsley-maxlength="140" data-parsley-minlength="1"></textarea>
+                </div>
+            </form>
+        </div>
+    <div class="next-question absol down-arrow">
+        @include('app/partials/icons/down-arrow')
+    </div>
+    </div>
 </div>
