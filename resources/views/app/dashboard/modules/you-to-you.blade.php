44288a962f58f61139e9282e3a42378e9388c23b
James Hopkins <james.hopkins@gladeye.co.nz>
2015-10-06 14:33:46 +1300

    responsive video in module8

--- resources/views/app/dashboard/modules/you-to-you.blade.php
+++ resources/views/app/dashboard/modules/you-to-you.blade.php
@@ -1,16 +1,18 @@
 <div class="content">
     <div class="inner-padding">
         <div class="content-width">
+            <h1 class="title">
+                Here I am
+            </h1>
 
-        <h1 class="title">
-            Here I am
-        </h1>
+            @if(isset($module->pivot->data[0]->letter))
+                <p class="dashboard">{{$module->pivot->data[0]->letter}}</p>
+            @elseif(isset($module->pivot->data[0]->video))
+                <div class="your-video">
+                    {!! $module->pivot->data[0]->video->embed->html !!}
+                </div>
+            @endif
 
-        @if(isset($module->pivot->data[0]->letter))
-            <p class="dashboard">{{$module->pivot->data[0]->letter}}</p>
-        @elseif(isset($module->pivot->data[0]->video))
-            {!! $module->pivot->data[0]->video->embed->html !!}
-        @endif
         </div>
     </div>
 </div>
