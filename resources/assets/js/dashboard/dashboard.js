b48b607ea93ebffac75f7aad0b49a43167738d3e
Daniel Bonham <daniel@gladeye.co.nz>
2015-10-06 16:31:51 +1300

    fix a js error when module 1 isn't and the user visits the dashboard

--- resources/assets/js/dashboard/dashboard.js
+++ resources/assets/js/dashboard/dashboard.js
@@ -8,29 +8,34 @@ export default class Dashboard {
 
 
     setupAccordian() {
-        let $boomerang = this.dashboard.find('.module-boomerang')
-        let wordsRaw = JSON.parse($boomerang.find('.tagcloud-words').html());
-        let wordsWeights = {};
-        let words = [];
-        if(wordsRaw) {
-            for(let i = 0; i < wordsRaw.length; i++) {
-                let word = wordsRaw[i];
-                if(wordsWeights[word.toLowerCase()]) {
-                    wordsWeights[word.toLowerCase()]++;
-                } else {
-                    wordsWeights[word.toLowerCase()] = 1;
-                }
-            }
 
-            for(let word in wordsWeights) {
-                words.push({
-                    text: word,
-                    weight: wordsWeights[word]
-                })
+
+        let $boomerang = this.dashboard.find('.module-boomerang');
+        if($boomerang.find('.tagcloud-words').length > 0) {
+            let wordsRaw = JSON.parse($boomerang.find('.tagcloud-words').html());
+            let wordsWeights = {};
+            let words = [];
+            if(wordsRaw) {
+                for(let i = 0; i < wordsRaw.length; i++) {
+                    let word = wordsRaw[i];
+                    if(wordsWeights[word.toLowerCase()]) {
+                        wordsWeights[word.toLowerCase()]++;
+                    } else {
+                        wordsWeights[word.toLowerCase()] = 1;
+                    }
+                }
+
+                for(let word in wordsWeights) {
+                    words.push({
+                        text: word,
+                        weight: wordsWeights[word]
+                    })
+                }
+                $boomerang.find('.tagcloud').jQCloud(words, {
+                    autoResize :true});
             }
-            $boomerang.find('.tagcloud').jQCloud(words, {
-                autoResize :true});
         }
+
 
         this.dashboard.find('.is-unlocked.is-complete .header').click((e)  => {
             $(e.currentTarget).closest('.is-unlocked.is-complete').toggleClass('is-open');
