dc49cba49a8b3c998ce22d629055350ab9d1beb2
James Hopkins <james.hopkins@gladeye.co.nz>
2015-10-07 17:19:09 +1300

    un-stuck copy

--- resources/views/app/modules/un-stuck/congrats.blade.php
+++ resources/views/app/modules/un-stuck/congrats.blade.php
@@ -5,9 +5,10 @@
             <h1 class="plain">Well done!</h1>
                 <p>Would you like to share what you learnt in the <a href="{{route('modules.forum',[$module->slug])}}">Forum?</a></p>
 
-                <p>How are you going with your commitment of <span class="commitment__display"> {{$requiredModules['welcome']->data[0]->challenge}}</span>? If you have slipped up, start again today. Change is never a straight line.</p>
+                <p>How are you going with your commitment of <br />
+                <span class="commitment__display"> {{$requiredModules['welcome']->data[0]->challenge}}?</span> <br /></p>
 
-                <p>Click through to your <a href="{{route('dashboard')}}">Dashboard</a> to see the responses that have come through from Module One and read you Affirmations.</p>
+                <p>If you have slipped up, start again today. <br />Change is never a straight line.</p>
 
                 <div class="actions">
                     <a href="#" class="button" data-complete-module>What's next?</a>
@@ -24,9 +25,11 @@
                 @else
                     <p>Your <a href="{{route('modules.view', ['visualise'])}}">next module</a> is ready and waiting for you.</p>
                 @endif
-                <p>
-                    In the meantime <a href="{{route('dashboard')}}#module-{{$module->slug}}">keep an eye on your dashboard</a> for the qualities submitted by your friends and <a href="{{route('modules.forum',[$module->slug])}}">check in the forum</a> to find support and chat to others about getting to know your amazing self.
-                </p>
+
+                <p>Click through to your <a href="{{route('dashboard')}}#module-{{$module->slug}}">Dashboard</a> to read your affirmations and keep an eye out for the qualities submitted by your friends.</p>
+
+                <p>Check in the <a href="{{route('modules.forum',[$module->slug])}}">Forum</a> to find support and chat to others about getting to know your amazing self.</p>
+
             </div>
         </div>
     </div>
