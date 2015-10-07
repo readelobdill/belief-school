4852013428224079d497f1ad562bb578ec5ede85
James Hopkins <james.hopkins@gladeye.co.nz>
2015-10-07 17:19:57 +1300

    fear-courage copy

--- resources/views/app/modules/fear-courage/response.blade.php
+++ resources/views/app/modules/fear-courage/response.blade.php
@@ -1,10 +1,15 @@
 <div class="inner">
     <div class="content">
         <h1 class="plain">So Proud of You</h1>
-        <p>On the other side of fear is courage. There is no downside to courage. When you act courageously, even if the outcome isn’t perfect you will feel powerful. You get to see who you really are and discover that you are much stronger than you thought.</p>
-        <p>This is where you leave the comfort of your screen and you take action in the big wide world. This is the ONLY way change will happen.  We Promise you, do these three things, do them with courage, feel the fear and do them anyway, you will change your world.</p>
-        <p>Many of you are going to stop right here – is that going to be you?</p>
-        <p><strong>I didn’t think so! No one can do these press ups for you. Choose courage, choose you.</strong></p>
+
+        <p>On the other side of fear is courage. There is no downside to courage. When you act courageously, even if the outcome isn’t perfect, you will feel powerful. You get to see who you really are and discover that you are much stronger than you thought.</p>
+
+        <p>Now is the time to leave the comfort of your screen and take action in the big wide world. This is the ONLY way change will happen. We promise you, do these three things, do them with courage, <b>feel the fear and do them anyway, and you will change your world.</b></p>
+
+        <p>Some of you are going to stop right here – is that going to be you?</p>
+
+        <p><b>I didn’t think so! No one can do these press ups for you. Choose courage, choose you.</b></p>
+
         <p>Take some pictures of your bravery if you can, we’d love to see them in the Forum!</p>
 
         <ul class="accordion single">
@@ -17,23 +22,26 @@
                     </div>
                 </h2>
                 <div class="answer">
-                    <p>Our comfort zone can show up in many ways. It’s sneaky, and its job is to keep us exactly where we are – safe and comfy.</p>
+                    <p>Your comfort zone can show up in many ways. It’s sneaky, and its job is to keep you exactly where you are – safe and comfy.</p>
+
                     <h3>It can show up as:</h3>
+
                     <ul class="list--simple">
                         <li>I don’t have time to do three things, I’ll just do one</li>
                         <li>I’ll just make something up and come back to this later</li>
-                        <li>There’s nothing I’m really frightened off, this doesn’t apply to me</li>
-                        <li>It’s too hard, I quite, but I won’t really say I’m quitting, I’ll just let it go</li>
-                        <li>I don’t have time this week, I’ll get to it next week</li>
+                        <li>There’s nothing I’m really frightened of, this doesn’t apply to me</li>
+                        <li>It’s too hard, I quit, but I won’t say I’m quitting, I just won’t do anything</li>
                     </ul>
-                    And many more just like this. If you want to build your belief in yourself, do not listen to this voice. Recognise it for what it is and complete this exercise, full out, for you, nothing less.
 
+                    <p>And many more like this. If you want to build belief in yourself, do not listen to this voice. </p>
                 </div>
             </li>
         </ul>
 
-        <p>In the spaces below write out how you felt as you faced your fear and did it anyway!</p>
-        <p>It is these feelings that will be saved to your Dashboard and will be your resource to call on in the future.</p>
+        <p>To complete this task, in the spaces below, write out how you felt as you faced your fear and did it anyway!</p>
+
+        <p>These feelings will be saved to your Dashboard, they’ll be a great resource to call on in the future, when fear raises it head.</p>
+
         <p>Stuck? Jump into the <a href="{{route('modules.forum', [$module->slug])}}">forum</a> for encouragement.</p>
         <form action="" class="positive-affirmations">
             <div class="positive-affirmations-fields">
