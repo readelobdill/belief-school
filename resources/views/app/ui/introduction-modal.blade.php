7e99de881739269924114325aab0f3e43b470087
Daniel Bonham <daniel@gladeye.co.nz>
2015-10-09 10:01:40 +1300

    updated introduction video modal

--- resources/views/app/ui/introduction-modal.blade.php
+++ resources/views/app/ui/introduction-modal.blade.php
@@ -1,4 +1,4 @@
 <div class="remodal" data-remodal-id="introduction">
     <button data-remodal-action="close" class="remodal-close"></button>
-    <iframe src="https://player.vimeo.com/video/136063687" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <iframe src="https://player.vimeo.com/video/141373491" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 </div>
