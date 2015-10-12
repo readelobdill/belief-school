1ec0efc92c040784c6cc3fd178b8d70feb53ca32
James Hopkins <james.hopkins@gladeye.co.nz>
2015-10-12 16:52:30 +1300

    payment failed copy.

--- resources/views/app/payments/failed.blade.php
+++ resources/views/app/payments/failed.blade.php
@@ -1,6 +1,19 @@
 @extends('app.layout')
 
 @section('content')
-    Testing
+    <div class="container non-mod-page">
+        <div class="inner">
+        <header>
+            <h2 class="title">[Sorry, your transaction was not succesful]</h2>
+        </header>
+            <section class="payment-failure">
+                <div class="inner">
+                    <div class="content">
 
+                        [Your credit card can not be authenticated: 2000 : The Authorisation was Declined by the bank.]
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
 @endsection
