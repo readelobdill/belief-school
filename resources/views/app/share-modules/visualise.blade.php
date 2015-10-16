473081832a554492be5a07d07f8529f67ee36941
Ken Vu <ken@gladeye.co.nz>
2015-10-16 16:30:43 +1300

    Fix og:type for `share-visualise`

--- resources/views/app/share-modules/visualise.blade.php
+++ resources/views/app/share-modules/visualise.blade.php
@@ -3,7 +3,7 @@
     <meta property="og:title" content="My Beautiful Life." />
     <meta property="og:site_name" content="Belief School" />
     <meta property="og:url" content="{{URL::current()}}" />
-    <meta property="og:type" content="image" />
+    <meta property="og:type" content="article" />
     <meta property="og:description" content="This is a collection of images that fill me with joy, inspire me and show me how much I am loved. I created this at Belief School." />
     <meta property="og:image" content="{{route('dreamboard.show', [$module->pivot->secret])}}?fb=1" />
 
