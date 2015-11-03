414735b64b7e356285bf81e0a9a1bb8fc521f0b5
James Hopkins <james.hopkins@gladeye.co.nz>
2015-11-03 16:22:26 +1300

    Swapped headings around as per request. and removed "..."

--- resources/views/app/dashboard/modules/gratitude.blade.php
+++ resources/views/app/dashboard/modules/gratitude.blade.php
@@ -7,12 +7,12 @@
         <ul class="experience">
             @foreach($module->pivot->data[0] as $key => $experience)
                 <li>
-                    <span>I am grateful for... </span> {{ $experience }}
+                    <span>I am grateful for</span> {{ $experience }}
                 </li>
             @endforeach
         </ul>
 
-        <h2 class="title">After I sent my letter of gratitude I felt...</h2>
+        <h2 class="title">After I sent my letter of gratitude I felt</h2>
         <p class="dashboard">
             {{ $module->pivot->data[1]->letter }}
         </p>
