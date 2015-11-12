795f0291ae6511f06180d22ae2919d16e7ae4456
Daniel Bonham <daniel@gladeye.co.nz>
2015-11-12 17:33:23 +1300

    fix issue with sharing video

--- resources/views/app/share-modules/you-to-you.blade.php
+++ resources/views/app/share-modules/you-to-you.blade.php
@@ -5,9 +5,7 @@
     Here I am
 @endsection
 @section('metadata')
-<?php
-$id = explode('/', $module->pivot->data[0]->video->uri)[2];
-?>
+
 
 <meta property="og:title" content="Here I am" />
 <meta property="og:site_name" content="Belief School" />
@@ -15,11 +13,11 @@ $id = explode('/', $module->pivot->data[0]->video->uri)[2];
 <meta property="og:type" content="article" />
 <meta property="og:description" content="" />
 
-<meta property="og:image" content="https://i.vimeocdn.com/video/default_1280x720" />
+<meta property="og:image" content="{{asset('uploads/you-to-you/'.$module->pivot->user->id . '/' . $module->pivot->data[0]->image)}}" />
 <meta property="og:image:height" content="720" />
 <meta property="og:image:width" content="1280" />
 
-<meta property="og:video:url" content="{{asset('uploads/you-to-you/'.Auth::user()->id . '/' . $module->pivot->data[0]->localVideo)}}" />
+<meta property="og:video:url" content="{{asset('uploads/you-to-you/'.$module->pivot->user->id. '/' . $module->pivot->data[0]->localVideo)}}" />
 <meta property="og:video:width" content="1280" />
 <meta property="og:video:height" content="720" />
 <meta property="og:video:type" content="video/mp4" />
@@ -32,7 +30,7 @@ $id = explode('/', $module->pivot->data[0]->video->uri)[2];
         Here I am
     </h1>
     <div class="your-video">
-        <video autoplay controls src="{{asset('uploads/you-to-you/'.Auth::user()->id . '/' . $module->pivot->data[0]->localVideo)}}"></video>
+        <video autoplay controls src="{{asset('uploads/you-to-you/'.$module->pivot->user->id . '/' . $module->pivot->data[0]->localVideo)}}"></video>
     </div>
 </div>
 @endsection
