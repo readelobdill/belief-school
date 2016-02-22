fae66f308248c4123b661270e97f176e0b1f03cc
James Hopkins <james.hopkins@gladeye.co.nz>
2016-02-22 17:27:12 +1300

    layout of courses on payment intro page

--- resources/views/app/modules/home/details.blade.php
+++ resources/views/app/modules/home/details.blade.php
@@ -14,14 +14,44 @@
             Are you interested in laying down a foundation of self-belief to create the outcomes you want in your life?
         </blockquote>
 
-        <div class="actions">
-            <a href="{{route('payment')}}" class="button" >Yes I want to start Belief School</a>
+    </div>
+
+    <div class="course-container">
+        <div class="course-container__column">
+            <h5 class="course-container__title">Belief School</h5>
+
+            <ul>
+                <li>Full Access to Belief School</li>
+                <li>Self-discipline exercises</li>
+                <li>Unlock a Belief School Manifesto on completion of the course</li>
+                <li>Access to the Belief School Community Forum</li>
+            </ul>
+            <div class="actions">
+                <a href="{{route('payment')}}" class="button">Start Belief School</a>
+            </div>
         </div>
+
+        <div class="course-container__column">
+            <h5 class="course-container__title">Belief School Coached</h5>
+            <ul>
+                <li>Work with Paula Gosney</li>
+                <li>Twice weekly live webinars with Paula Gosney</li>
+                <li>Closed community Facebook group</li>
+                <li>Weekly Q&amp;A sessions</li>
+                <li>Additional tools and insights to support your journey</li>
+            </ul>
+            <div class="actions">
+                <a href="{{route('payment')}}" class="button">Belief School Coached</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
 
         <p>You can buy a gazillion programs, sign up the best coaches in the world, even win Lotto but, if you don’t believe in yourself and your self-worth, your life will stay the same.</p>
 
         <div class="actions">
             <a href="{{ route('about') }}" class="button small">Do you need to know more before you say yes?</a>
         </div>
-    </div>
+   </div>
 </div>
