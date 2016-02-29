1cd8b65c22a9dc91b7ade12499877a076b07472e
Daniel Bonham <daniel@gladeye.co.nz>
2016-03-01 11:41:52 +1300

    change title in admin

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>AdminLTE 2 | Dashboard</title>
+    <title>Belief School | Dashboard</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <meta name="csrf-token" content="{{csrf_token()}}"/>
     <!-- Bootstrap 3.3.2 -->
