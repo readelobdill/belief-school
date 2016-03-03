04ca3478d47d072f7ce18bcf86336c6ff45d12f3
Daniel Bonham <daniel@gladeye.co.nz>
2016-03-03 14:51:59 +1300

    copy change

--- resources/views/app/ui/help-modal.blade.php
+++ resources/views/app/ui/help-modal.blade.php
@@ -7,12 +7,11 @@
 	    	<p><b>Here are some tips for filming and uploading your video:</b></p>
 
 	    	<ul class="help-modal__list">
-				<li>Uploading video can take a while especially on slower connections, it’s a good idea to keep your message to about 3 minutes, or under 1Gb</li>
-				<li>Film in landscape ▭ rather than portrait ▯</li>
-
+				<li>Uploading video can take a while especially on slower connections; you will need to keep your file size below 1GB.</li>
+				<li>If you are struggling to upload, consider recording on your computer, and we encourage you to use a wifi connection.</li>
 				<li>Files can be uploaded as either MP4 or MOV</li>
-				<li>If you have any other questions you can check out the forum <a href="{{route('modules.forum',[$module->slug])}}" target="_blank">here</a>
-				Or reach out here <a href="mailto:contact@beliefschool.com">contact@beliefschool.com</a></li>
+				<li>If you need support or have any questions, head to the <a href="{{route('modules.forum',[$module->slug])}}" target="_blank">forum!</a>
+					Or email us here <a href="mailto:contact@beliefschool.com">contact@beliefschool.com</a></li>
 			</ul>
 		</div>
 	</div>
