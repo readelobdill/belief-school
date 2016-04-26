ed903c03dc38e25e44be7eb34a6bfaecf787ef78
Daniel Bonham <daniel@gladeye.co.nz>
2016-04-27 09:14:40 +1200

    fix smart quotes

--- resources/views/emails/boomerang.blade.php
+++ resources/views/emails/boomerang.blade.php
@@ -1,10 +1,10 @@
 Dear Friend,
 
-I’m completing an online program called Belief School. It’s a personal development program helping me understand how I operate in the world. No, this is not spam, please text me if you need to check.
+I'm completing an online program called Belief School. It's a personal development program helping me understand how I operate in the world. No, this is not spam, please text me if you need to check.
 
-I’m sending you this email because I value your opinion and trust that you will answer the simple question, honestly and with my best interest at heart.
+I'm sending you this email because I value your opinion and trust that you will answer the simple question, honestly and with my best interest at heart.
 
-Clicking on the link will take you to a page on the Belief School website, you’ll be asked to input three words that describe my best qualities. The answers will be delivered to me anonymously, mixed up with responses from friends, family and colleagues.
+Clicking on the link will take you to a page on the Belief School website, you'll be asked to input three words that describe my best qualities. The answers will be delivered to me anonymously, mixed up with responses from friends, family and colleagues.
 
 Please keep your answers to a single word or simple phrase.
 
