4bdf954ff194d656cf77d83a91eead64ab117893
Reade <readelobdill@aim.com>
2016-05-04 17:24:10 -0700

    change app.php config

--- config/app.php
+++ config/app.php
@@ -148,7 +148,7 @@ return [
         /*
          * Other Service Providers...
          */
-        'Lanz\Commentable\CommentableServiceProvider',
+        'Slynova\Commentable\CommentableServiceProvider',
         'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
 
 		'App\Providers\MenuServiceProvider',
