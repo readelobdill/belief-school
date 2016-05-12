6aa504f0095cb207b4c2196df1c8bd3a330f4e67
Reade <readelobdill@aim.com>
2016-05-11 17:57:01 -0700

    change boomerang email template

--- resources/views/app/modules/boomerang/email.blade.php
+++ resources/views/app/modules/boomerang/email.blade.php
@@ -10,17 +10,18 @@
         <div class="subject-row">Subject</div>
     </div>
     <div class="message">
+
         <p>Dear Friend,</p>
 
-        <p>I’m completing an online program called Belief School. It’s a personal development program helping me understand how I operate in the world. No, this is not spam, please text me if you need to check.</p>
+        <p>I'm completing an online program called Belief School. It's a personal development program helping me understand how I operate in the world. No, this is not spam, please message me if you need to check.</p>
 
-        <p>I’m sending you this email because I value your opinion and trust that you will answer the simple question, honestly and with my best interest at heart.</p>
+        <p>Clicking on the link below will take you to a page on the Belief School website, you'll be asked to input three words that describe my best qualities. The answers will be delivered to me anonymously, mixed up with responses from friends, family and colleagues.</p>
 
-        <p>Clicking on the link will take you to a page on the Belief School website, you’ll be asked to input three words that describe my best qualities. The answers will be delivered to me anonymously, mixed up with responses from friends, family and colleagues.</p>
+        <p>I'm sending you this email because I value your opinion and trust that you will answer the simple question honestly, focusing on my strengths.</p>
 
         <p>Please keep your answers to a single word or simple phrase.</p>
 
-        <p>This will only take one minute yet will have a BIG impact for me. Thanks for taking the time, it really does matter.</p>
+        <p>This will only take one minute and will have a BIG impact for me. Thanks for taking the time, it really does matter.</p>
 
         <p>With love,<br /><span class="users-name">{{Auth::user()->first_name}}</span></p>
 
--- resources/views/emails/boomerang.blade.php
+++ resources/views/emails/boomerang.blade.php
@@ -1,14 +1,14 @@
 Dear Friend,
 
-I'm completing an online program called Belief School. It's a personal development program helping me understand how I operate in the world. No, this is not spam, please text me if you need to check.
+I'm completing an online program called Belief School. It's a personal development program helping me understand how I operate in the world. No, this is not spam, please message me if you need to check.
 
-I'm sending you this email because I value your opinion and trust that you will answer the simple question, honestly and with my best interest at heart.
+Clicking on the link below will take you to a page on the Belief School website, you'll be asked to input three words that describe my best qualities. The answers will be delivered to me anonymously, mixed up with responses from friends, family and colleagues.
 
-Clicking on the link will take you to a page on the Belief School website, you'll be asked to input three words that describe my best qualities. The answers will be delivered to me anonymously, mixed up with responses from friends, family and colleagues.
+I'm sending you this email because I value your opinion and trust that you will answer the simple question honestly, focusing on my strengths.
 
 Please keep your answers to a single word or simple phrase.
 
-This will only take one minute yet will have a BIG impact for me. Thanks for taking the time, it really does matter.
- 
+This will only take one minute and will have a BIG impact for me. Thanks for taking the time, it really does matter.
+
 With love,
 {{Auth::user()->first_name}}
